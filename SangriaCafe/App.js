cc80fd3ff6515201849cbd49e709ba1e221a5d71
edhernandez04 <mchh.004@gmail.com>
2020-07-21 19:40:56 -0400

    logo & topBar

--- SangriaCafe/App.js
+++ SangriaCafe/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
 import Home from './screens/home'
 
 const App = () => {
   return (
-    <PaperProvider>
       <Home />
-    </PaperProvider>
   );
 };
 
--- SangriaCafe/screens/home.js
+++ SangriaCafe/screens/home.js
@@ -1,47 +1,58 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, ScrollView, View, Image, Dimensions, Button, Text } from 'react-native';
-import TabNav from '../components/TabNav.js'
+import { SafeAreaView, StyleSheet, View, Image, Dimensions, Button, Text } from 'react-native';
 
-class Home extends React.Component {
+export default class Home extends React.Component {
   
   render(){
     return (
-      <>
+      <View>
         <SafeAreaView>
-          <View style={styles.topBar}>
-            <Image source={ require('../assets/sangria_logo.png')} style={styles.logo}/>
+          <View style={styles.topContainer}>
+            <Image source={require('../assets/sangria_logo.png')} style={styles.logo}/>
           </View>
           <View style={styles.userBar}>
+            <View style={styles.userIcon}>
+              <Text style={{fontSize:20}}>PardiHardi</Text>
+            </View>
+            <View style={styles.userIcon}>
+              <Text style={{fontSize:20}}>750 </Text>
+              <Image source={require('../assets/toro.png')} style={styles.toro}/>
+            </View>
           </View>
         </SafeAreaView>
-      </>
-    );
-  };
-
+      </View>
+    )
   }
 
-const screen = Dimensions.get('window')
+}
 
+const screen = Dimensions.get('window')
 const styles = StyleSheet.create({
-    topBar: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      paddingTop: 10
+    topContainer: {
+      alignItems: 'center',
+      paddingTop: 2,
+    },
+    logo: {
+        height: screen.width / 4,
+        width: screen.width / 1.7,
+    },
+    toro: {
+      height: 25,
+      width: 25
     },
     userBar: {
       flexDirection: 'row',
       justifyContent: 'space-between',
+    },
+    userIcon: {
+      flexDirection: 'row',
       padding: 10
     },
-    userName: {
-      fontSize: 14,
-      color: '#2f354b',
-      textAlign: 'center'
-    },
-    logo: {
-        height: screen.width / 4,
-        width: screen.width / 1.7
+    nav: {
+      width: '100%',
+      height: 70,
+      backgroundColor: '#EE5300',
+      justifyContent: 'center',
+      top: screen.height / 2
     }
-});
-
-export default Home;
+})
