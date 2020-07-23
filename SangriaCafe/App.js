69b110cbfac24fde97e682d6269e68930f6c0c2e
edhernandez04 <mchh.004@gmail.com>
2020-07-23 13:48:05 -0400

    navigation

--- SangriaCafe/App.js
+++ SangriaCafe/App.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import { View, Text, Image } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './screens/home'
+
+function HomeScreen() {
+  return (
+    <Home />
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function OrderScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Order</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-      <Home />
-  );
-};
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+            if (route.name === 'Home') {
+              iconName = focused ? 'https://cdn2.iconfinder.com/data/icons/font-awesome/1792/home-512.png' : 'https://cdn2.iconfinder.com/data/icons/font-awesome/1792/home-512.png'
+            } else if (route.name === 'Order') {
+              iconName = focused ? 'https://image.flaticon.com/icons/png/512/45/45552.png' : 'https://image.flaticon.com/icons/png/512/45/45552.png'
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'https://img.pngio.com/profile-icon-png-image-free-download-searchpngcom-profile-icon-png-673_673.png' : 'https://img.pngio.com/profile-icon-png-image-free-download-searchpngcom-profile-icon-png-673_673.png'
+            }
+          return <Image source={{uri:iconName}} size={size} color={color} style={{height: 22, width: 22}}/>
+        },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Order" component={OrderScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
 
 export default App;
--- SangriaCafe/screens/home.js
+++ SangriaCafe/screens/home.js
@@ -1,28 +1,27 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, View, Image, Dimensions, Button, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Image, Dimensions, Text } from 'react-native'
 
 export default class Home extends React.Component {
-  
+
   render(){
     return (
-      <View>
+      <>
         <SafeAreaView>
           <View style={styles.topContainer}>
             <Image source={require('../assets/sangria_logo.png')} style={styles.logo}/>
           </View>
           <View style={styles.userBar}>
-            <View style={styles.userIcon}>
-              <Text style={{fontSize:20}}>PardiHardi</Text>
-            </View>
-            <View style={styles.userIcon}>
-              <Text style={{fontSize:20}}>750 </Text>
-              <Image source={require('../assets/toro.png')} style={styles.toro}/>
-            </View>
+          <View style={styles.userIcon}>
+            <Text style={{fontSize:20}}>PardiHardi</Text>
+          </View>
+          <View style={styles.userIcon}>
+            <Text style={{fontSize:20}}>750 </Text>
+            <Image source={require('../assets/toro.png')} style={styles.toro}/>
+          </View>
           </View>
         </SafeAreaView>
-      </View>
-    )
-  }
+      </>
+    )}
 
 }
 
@@ -37,8 +36,8 @@ const styles = StyleSheet.create({
         width: screen.width / 1.7,
     },
     toro: {
-      height: 25,
-      width: 25
+      height: 28,
+      width: 28
     },
     userBar: {
       flexDirection: 'row',
@@ -48,11 +47,8 @@ const styles = StyleSheet.create({
       flexDirection: 'row',
       padding: 10
     },
-    nav: {
-      width: '100%',
-      height: 70,
-      backgroundColor: '#EE5300',
-      justifyContent: 'center',
-      top: screen.height / 2
+    frontImage: {
+      height: screen.height / 3,
+      width: screen.width
     }
 })
