4c29cd2f457a98a33fb24d92ab7cfbd4eb85ef78
edhernandez04 <mchh.004@gmail.com>
2020-09-21 15:46:21 -0400

    cleanup post order raw body text

--- SangriaCafe/screens/cart.js
+++ SangriaCafe/screens/cart.js
@@ -31,30 +31,13 @@ const Cart = props => {
             'Store-Sub-ID':"2296-1C2A"
         }
         
-        const postOrderBody = {
-            "EmployeeID": 1000000000000000001,
-            "OrderType": orderType, // int, not null 3=TakeOut, 5=Delivery
-            "GuestCount": 1,
-            "CustomerName": null, // string, null, up to 100 char, CustomerName for customer
-            "Telephone": null, // string, null, up to 20 char, numbers and dash '-' only, valid formats: ###-###-####, ########, Telephone for customer
-            "Address": null, // string, null, up to 100 char, address for customer
-            "PostalCode": null, // string, null, up to 10 char, PostalCode for customer
-            "City": 'Bronx', // string, null, up to 80 char, City for PostalCode
-            "State": 'New York', // string, null, up to 20 char, State for PostalCode
-            "DeliveryCharge": deliveryFee(), // float, null, delivery charge assessed to customer for this order
-            "OrderGratuityPercent": (tip/total())*100, // float, null, order gratuity applied toward this order
-            "AutoPrint": false, // bool, if true = order will auto print to kitchen but a notification is sent; if false = order will not auto print to kitchen
-            "SystemPrint": false, // bool, if true = order will print to kitchen based on pos config AutoPrintNonPaidISVOrdersToKitchen; if false = order will not print to kitchen
-            "OrderDetails": props.foodCart.map( food => {
-                return ({
-                    "ItemID" : food.item.name,
-                    "Qty" : food.quantity,
-                    "UnitPrice" : food.item.details.price,
-                    "LineNote" : food.instruction,
-                    "CreatedByEmployeeID" : 1000000000000000001
-                })
-            })
-        }
+        const postOrderBody =
+            `{\n	\"EmployeeID\": 1000000000000000001, // bigint, not null\n	\"OrderType\": ${orderType}, // int, not null, 1=DineIn, 2=Bar, 3=TakeOut, 4=DriveThru, 5=Delivery, 6=Retail\n    \"GuestCount\": 1, // int, not null, 1 ~ 99\n	\"SeatingID\": null, // bigint, null\n	\"BarTabName\": null, // string, null, up to 100 char, bar tab order type only\n	\"CustomerID\": null, // bigint, null\n	\"CustomerName\": ${currentUser.name}, // string, null, up to 100 char, CustomerName for customer\n	\"Telephone\": ${currentUser.phoneNumber}, // string, null, up to 20 char, numbers and dash '-' only, valid formats: ###-###-####, ########, Telephone for customer\n    \"Email\": null, // string, null, up to 100 char, Email for customer\n	\"Address\": ${currentUser.address}, // string, null, up to 100 char, address for customer\n	\"PostalCode\": ${currentUser.postalCode}, // string, null, up to 10 char, PostalCode for customer\n	\"City\": Bronx, // string, null, up to 80 char, City for PostalCode\n    \"State\": New York, // string, null, up to 20 char, State for PostalCode\n    \"CustomerPickupName\": null, // string, null, up to 100 char\n    \"OrderDiscountID\": null,  // [bigint] NULL\n    \"CashPromoEmployeeID\": null, // bigint, null, a cash promotional discount applied by an employee, this field indicates the employee who applied it\n    \"CashPromoAmountApplied\": null, // float, null, cash promotional discount applied to this order\n    \"OrderSurchargeID\": null,  // bigint, null\n    \"DeliveryCharge\": ${deliveryFee()}, // float, null, delivery charge assessed to customer for this order\n    \"OrderGratuityPercent\": ${(tip/total())*100}, // float, null, order gratuity applied toward this order\n    \"CashGratuityAmountUsed\": null, // float, null, cash gratuity applied toward this order\n    \"OrderNote\": null, // string, null, up to 40 char, order level note or comment\n    \"AutoPrint\": false, // bool, if true = order will auto print to kitchen but a notification is sent; if false = order will not auto print to kitchen\n    \"SystemPrint\": false, // bool, if true = order will print to kitchen based on pos config AutoPrintNonPaidISVOrdersToKitchen; if false = order will not print to kitchen\n    \"FutureDateHoldUntilTime\": \"2018-12-10 20:00\", // datetime, null, Future Date Used Only, order is held until this date and time, format is yyyy-mm-dd hh:mm with hh in 24 hour notation\n \n    \"OrderDetails\": [\n   ${props.foodCart.map( food => {
+            return `{\n    	\"ItemID\": ${food.item.name}, // bigint, not null\n        \"SeatNumber\": null, // int, null\n        \"Qty\": ${food.quantity}, // float, not null, only weighted item can have fractions, 3 decimal places for weighted item, non weighted item must be integer\n        \"UnitPrice\": ${food.item.details.price}, // float, null\n        \"LineDiscountID\": null, // bigint, null\n        \"LineNote\": ${food.instruction}, // string, null, up to 100 char\n        \"CreatedByEmployeeID\": 1000000000000000001, // bigint, not null, order detail created by this employee\n        \"SameDateHoldUntilTime\": \"2018-12-10 20:00\", // datetime, null, Same Date Hold Used Only, order item is held until this date and time, format is yyyy-mm-dd hh:mm with hh in 24 hour notation\n\n \"OrderDetailModifiers\": [\n          {\n            \"ModifierID\": 1000000000000000001, // bigint, not null\n            \"ModifierPrice\": 0.25, // float, null, modifier price to add on\n            \"PortionType\": null, // int, null, valid values are: null or 0 means no portion type; 1 = for whole portion; 2 = for halves portion; 3 = for thirds portion (portion is used in pizza ordering)\n            \"PortionSection\": null, // int, null, conditionally required if PortionType is 2 or 3; indicates portion section: 1=Whole, 2=FirstHalf, 3=SecondHalf, 4=FirstThirds, 5=SecondThirds, 6=ThirdThirds\n            \"ModifierType\": null, // int, null, null means 1, valid values are: null or 1 = for forced modifier; 2 = for advanced modifiers; 3 = for pizza modifier\n            \"ModifierGroupNumber\": null, // int, null, null means group 1, valid values 1 to 10\n          },\n          ... // more order detail modifiers\n        ],\n        \"TagAlongItems\": [ // If menu item includes tag along items, then ISV app must include tag along items along with associated modifiers during order entry and order submit\n          {\n            \"ItemID\": 1000000000000000004, // bigint, not null\n            \"OrderDetailModifiers\": [\n              {\n                \"ModifierID\": 1000000000000000291, // bigint, not null\n                \"ModifierPrice\":5, // float, null, modifier price to add on\n                \"PortionType\": null, // int, null, valid values are: null or 0 means no portion type; 1 = for whole portion; 2 = for halves portion; 3 = for thirds portion (portion is used in pizza ordering)\n                \"PortionSection\": null, // int, null, conditionally required if PortionType is 2 or 3; indicates portion section: 1=Whole, 2=FirstHalf, 3=SecondHalf, 4=FirstThirds, 5=SecondThirds, 6=ThirdThirds\n                \"ModifierType\": 1, // int, null, null means 1, valid values are: null or 1 = for forced modifier; 2 = for advanced modifiers; 3 = for pizza modifier\n                \"ModifierGroupNumber\": 1, // int, null, null means group 1, valid values 1 to 10\n              },\n            ]\n          },\n        ]\n      },\n      ... // more order detail records for this order\n    ]\n}`
+        })}`
+
+        // valid formats: ###-###-####, ######## !!! NEED FUUNCTION TO REFORMAT
+        // add POSTAL CODE TO SETTINGS FORM
 
         const requestOptions = {
             method: 'POST',
@@ -63,7 +46,7 @@ const Cart = props => {
             redirect: 'follow'
         }
 
-        await fetch("https://sandbox.aldelo.io/v1/boarding/connected/20181202/20181203", requestOptions)
+        await fetch("https://sandbox.aldelo.io/v1/order", requestOptions)
             .then(response => response.text())
             .then(result => console.log(result))
             .catch(error => console.log('error', error))
