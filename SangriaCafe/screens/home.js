35255cc4be0601423e1b383e93393d1741af15d4
edhernandez04 <mchh.004@gmail.com>
2020-09-25 17:46:27 -0400

    call & map button on HOME

--- SangriaCafe/screens/home.js
+++ SangriaCafe/screens/home.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, View, Image, Dimensions, Text, ScrollView } from 'react-native'
+import { SafeAreaView, StyleSheet, View, Image, Dimensions, Text, ScrollView, Linking } from 'react-native'
+import { Button } from 'react-native-paper'
 import Card from '../components/instaCard'
 import Map from '../components/map'
 
@@ -21,6 +22,10 @@ export default class Home extends React.Component {
               <Text style={styles.storeHours}>Bar: 12PM - 2AM</Text>
               <Text style={styles.storeHours}>HH: 3PM-7PM</Text>
             </View>
+          </View>
+          <View style={{flexDirection: 'row', justifyContent: 'space-around', backgroundColor: 'tomato'}}>
+            <Button color='white' onPress={() => Linking.openURL('tel:3478436486')}>Call</Button>
+            <Button color='white' onPress={() => Linking.openURL('https://www.google.com/maps/place/2085+Bartow+Ave,+The+Bronx,+NY+10475/@40.8697287,-73.8299331,17z/data=!3m1!4b1!4m5!3m4!1s0x89c28cb85da85129:0xf447e9e1854d3292!8m2!3d40.8697287!4d-73.8277444')}>Maps</Button>
           </View>
           <View style={{margin:5}}>
             <Card/>
