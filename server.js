3363f914e7bf78c069e1a7a95e2332eb92326b1a
edhernandez04 <mchh.004@gmail.com>
2020-10-16 16:32:18 -0400

    maybe better w/o sever.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const publicPath = path.join(__dirname);
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'App.js'));
+  res.render(path.join(publicPath, 'index.js'));
 });
 app.listen(port, () => {
   console.log('Server is up!');
