a7eab632adf032165c903d4499364b6decc5e2b1
edhernandez04 <mchh.004@gmail.com>
2020-10-16 14:03:36 -0400

    open to all paths

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'index')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.js'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index', 'index.js'));
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
