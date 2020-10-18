0ae6ef3b89f154f49a3252aadc6122962417f116
edhernandez04 <mchh.004@gmail.com>
2020-10-18 13:52:44 -0400

    revert

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App.js';
-import React from 'react';
-import ReactDom from 'react-dom';
 
 AppRegistry.registerComponent("SangriaCafe", () => App);
-ReactDom.render(<App />, document.getElementById("root"));
+// AppRegistry.runApplication('App', { rootTag: document.getElementById('react-root') });
 
