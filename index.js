c85b6def20a44ff325771eb01b6140fea3946da5
edhernandez04 <mchh.004@gmail.com>
2020-10-17 22:57:37 -0400

    yarn web

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App.js';
+import React from 'react';
+import ReactDom from 'react-dom';
 
 AppRegistry.registerComponent("SangriaCafe", () => App);
+ReactDom.render(<App />, document.getElementById("root"));
+
