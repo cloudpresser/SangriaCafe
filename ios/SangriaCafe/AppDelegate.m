a9479ec9b48624a6970948b9279388cbb258e665
Ed <MCHH.004@gmail.com>
2020-11-16 10:49:53 -0500

    styling reciept cart section

--- ios/SangriaCafe/AppDelegate.m
+++ ios/SangriaCafe/AppDelegate.m
@@ -1,7 +1,7 @@
 #import "AppDelegate.h"
 
 #import "RNGoogleSignin.h"
-
+#import <UserNotifications/UserNotifications.h>
 #import <Firebase.h>
 
 #import <React/RCTBridge.h>
@@ -53,6 +53,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [self requestPushNotificationPermissions];
     [FIRApp configure];
   return YES;
 }
@@ -69,6 +70,73 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
   [FBSDKAppEvents activateApp];
+}
+
+- (void)application:(UIApplication*)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)devToken
+{
+  // parse token bytes to string
+  const char *data = [devToken bytes];
+  NSMutableString *token = [NSMutableString string];
+  for (NSUInteger i = 0; i < [devToken length]; i++)
+  {
+    [token appendFormat:@"%02.2hhX", data[i]];
+  }
+  
+  // print the token in the console.
+  NSLog(@"Push Notification Token: %@", [token copy]);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+  // could not register a Push Notification token at this time.
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+  // app has received a push notification
+}
+
+- (void)requestPushNotificationPermissions
+{
+  // iOS 10+
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  [center getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+    
+    switch (settings.authorizationStatus)
+    {
+      // User hasn't accepted or rejected permissions yet. This block shows the allow/deny dialog
+      case UNAuthorizationStatusNotDetermined:
+      {
+        center.delegate = self;
+        [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error)
+         {
+           if(granted)
+           {
+             [[UIApplication sharedApplication] registerForRemoteNotifications];
+           }
+           else
+           {
+             // notify user to enable push notification permission in settings
+           }
+         }];
+        break;
+      }
+      // the user has denied the permission
+      case UNAuthorizationStatusDenied:
+      {
+        // notify user to enable push notification permission in settings
+        break;
+      }
+      // the user has accepted; Register a PN token
+      case UNAuthorizationStatusAuthorized:
+      {
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+        break;
+      }
+      default:
+        break;
+    }
+  }];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
--- screens/cart.js
+++ screens/cart.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -153,8 +153,8 @@ const Cart = (props) => {
         <ScrollView
           alwaysBounceVertical={true}
           showsVerticalScrollIndicator={false}
-          contentInset={{bottom: 100}}>
-          <View style={{padding: 10}}>
+          contentInset={{ bottom: 100 }}>
+          <View style={{ padding: 10 }}>
             <Switch
               thumbColor={orderType === 3 ? '#767577' : 'tomato'}
               onValueChange={() => flipOrderType()}
@@ -166,49 +166,49 @@ const Cart = (props) => {
           {props.foodCart.map((food) => {
             return (
               <View style={styles.recieptTheme} key={food.item[0]}>
-                <View style={{alignItems: 'flex-start', flexDirection: 'row'}}>
+                <View style={{ flexDirection: 'row' }}>
                   <Text>{food.quantity} </Text>
                   <Text> {food.item[0]}</Text>
                 </View>
-                <View style={{alignItems: 'flex-end'}}>
+                <Text style={styles.subText}>{food.instruction ? food.instruction : null}</Text>
+                <View style={{ alignItems: 'flex-end' }}>
                   <Text>${food.item[1].price * food.quantity}</Text>
-                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                     <Image
                       source={{
                         uri:
                           'https://firebasestorage.googleapis.com/v0/b/sangriacafe.appspot.com/o/assets%2Ftoro.png?alt=media&token=240fcdac-2e49-47e7-b3ea-8a2f93d4105e',
                       }}
-                      style={{height: 20, width: 20}}
+                      style={{ height: 20, width: 20 }}
                     />
                     <Text> {food.item[1].toros * food.quantity}</Text>
                   </View>
-                  <Text>{food.instruction ? food.instruction : null}</Text>
                   <TouchableOpacity onPress={() => props.removeFromCart(food)}>
-                    <Text style={{color: '#0645AD'}}>Remove</Text>
+                    <Text style={{ color: '#0645AD' }}>Remove</Text>
                   </TouchableOpacity>
                 </View>
               </View>
             );
           })}
 
-          <View style={{padding: 20}}>
+          <View style={{ padding: 20 }}>
             <View
-              style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+              style={{ justifyContent: 'space-between', flexDirection: 'row' }}>
               <Text>Subtotal</Text>
               <Text>${subtotal().toFixed(2)}</Text>
             </View>
             <View
-              style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+              style={{ justifyContent: 'space-between', flexDirection: 'row' }}>
               <Text>Delivery fee</Text>
               <Text>{deliveryFee() === 0 ? 'free' : '$' + 1.99}</Text>
             </View>
             <View
-              style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+              style={{ justifyContent: 'space-between', flexDirection: 'row' }}>
               <Text>Tax</Text>
               <Text>${salesTax().toFixed(2)}</Text>
             </View>
             <View
-              style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+              style={{ justifyContent: 'space-between', flexDirection: 'row' }}>
               <Text>Total</Text>
               <Text>${total().toFixed(2)}</Text>
             </View>
@@ -225,7 +225,7 @@ const Cart = (props) => {
                   uri:
                     'https://firebasestorage.googleapis.com/v0/b/sangriacafe.appspot.com/o/assets%2Ftoro.png?alt=media&token=240fcdac-2e49-47e7-b3ea-8a2f93d4105e',
                 }}
-                style={{height: 20, width: 20}}
+                style={{ height: 20, width: 20 }}
               />
               <Text> {toroTotal()}</Text>
             </View>
@@ -255,8 +255,8 @@ const Cart = (props) => {
                       onPress={() => tipHandler(0.15)}
                     />
                   ) : (
-                    <Button title={'15%'} onPress={() => tipHandler(0.15)} />
-                  )}
+                      <Button title={'15%'} onPress={() => tipHandler(0.15)} />
+                    )}
                   {tip / total() === 0.2 ? (
                     <Button
                       title={'20%'}
@@ -264,8 +264,8 @@ const Cart = (props) => {
                       onPress={() => tipHandler(0.2)}
                     />
                   ) : (
-                    <Button title={'20%'} onPress={() => tipHandler(0.2)} />
-                  )}
+                      <Button title={'20%'} onPress={() => tipHandler(0.2)} />
+                    )}
                   {tip / total() === 0.25 ? (
                     <Button
                       title={'25%'}
@@ -273,8 +273,8 @@ const Cart = (props) => {
                       onPress={() => tipHandler(0.25)}
                     />
                   ) : (
-                    <Button title={'25%'} onPress={() => tipHandler(0.25)} />
-                  )}
+                      <Button title={'25%'} onPress={() => tipHandler(0.25)} />
+                    )}
                   {tip / total() === 0.3 ? (
                     <Button
                       title={'30%'}
@@ -282,8 +282,8 @@ const Cart = (props) => {
                       onPress={() => tipHandler(0.3)}
                     />
                   ) : (
-                    <Button title={'30%'} onPress={() => tipHandler(0.3)} />
-                  )}
+                      <Button title={'30%'} onPress={() => tipHandler(0.3)} />
+                    )}
                   {tip === 0 ? (
                     <Button
                       title={'Cash'}
@@ -291,8 +291,8 @@ const Cart = (props) => {
                       onPress={() => tipHandler(0)}
                     />
                   ) : (
-                    <Button title={'Cash'} onPress={() => tipHandler(0)} />
-                  )}
+                      <Button title={'Cash'} onPress={() => tipHandler(0)} />
+                    )}
                 </View>
               </>
             ) : null}
@@ -300,7 +300,7 @@ const Cart = (props) => {
             <TouchableOpacity onPress={() => checkoutButtonPress()}>
               <View style={styles.checkoutButton}>
                 <Text
-                  style={{color: 'white', fontWeight: 'bold', fontSize: 17}}>
+                  style={{ color: 'white', fontWeight: 'bold', fontSize: 17 }}>
                   CHECKOUT ${(parseFloat(total()) + parseFloat(tip)).toFixed(2)}
                 </Text>
               </View>
@@ -310,29 +310,29 @@ const Cart = (props) => {
       </SafeAreaView>
     </>
   ) : (
-    <>
-      <SafeAreaView>
-        <View style={styles.topContainer}>
-          <Image
-            source={{
-              uri:
-                'https://firebasestorage.googleapis.com/v0/b/sangriacafe.appspot.com/o/assets%2Fsangria_logo.png?alt=media&token=65d5bb98-bcfc-4599-bc4c-395ac130212a',
-            }}
-            style={styles.logo}
-          />
-        </View>
-        <View style={styles.empty}>
-          <Image
-            source={{
-              uri:
-                'https://firebasestorage.googleapis.com/v0/b/sangriacafe.appspot.com/o/assets%2Fempty.png?alt=media&token=b86c1d0b-45df-4931-84bb-9299f68bc7f7',
-            }}
-            style={styles.emptyImage}
-          />
-        </View>
-      </SafeAreaView>
-    </>
-  );
+      <>
+        <SafeAreaView>
+          <View style={styles.topContainer}>
+            <Image
+              source={{
+                uri:
+                  'https://firebasestorage.googleapis.com/v0/b/sangriacafe.appspot.com/o/assets%2Fsangria_logo.png?alt=media&token=65d5bb98-bcfc-4599-bc4c-395ac130212a',
+              }}
+              style={styles.logo}
+            />
+          </View>
+          <View style={styles.empty}>
+            <Image
+              source={{
+                uri:
+                  'https://firebasestorage.googleapis.com/v0/b/sangriacafe.appspot.com/o/assets%2Fempty.png?alt=media&token=b86c1d0b-45df-4931-84bb-9299f68bc7f7',
+              }}
+              style={styles.emptyImage}
+            />
+          </View>
+        </SafeAreaView>
+      </>
+    );
 };
 
 export default Cart;
@@ -349,7 +349,6 @@ const styles = StyleSheet.create({
     width: screen.width / 1.7,
   },
   recieptTheme: {
-    flexDirection: 'row',
     justifyContent: 'space-between',
     padding: 10,
     backgroundColor: 'white',
@@ -381,4 +380,8 @@ const styles = StyleSheet.create({
   selectedButton: {
     color: 'tomato',
   },
+  subText: {
+    fontSize: 11,
+    fontStyle: 'italic'
+  }
 });
